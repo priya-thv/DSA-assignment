93c80bcb90f223632c6b850cba1df1643cc68786
priya <107272239+priya-thv@users.noreply.github.com>
2022-09-20 12:36:21 +0530

    Create inputoutput.cpp

---
+++ Assignment 1/inputoutput.cpp
@@ -0,0 +1,16 @@
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+
+#include<iostream>
+
+
+
+int main(){
+    int a(1);
+    int b(2);
+    int c(7);
+    int sum=a+b+c;
+    std::cout<<sum<<std::endl;
+    return 0;
+    
+}
