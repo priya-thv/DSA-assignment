45e17aa69873e81b8d43d4dcf61d796455e2e4a2
priya <107272239+priya-thv@users.noreply.github.com>
2022-09-20 12:39:15 +0530

    Create basicdatatype.cpp

---
+++ Assignment 1/basicdatatype.cpp
@@ -0,0 +1,22 @@
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int number(3);
+    long number2(12345678912345);
+    char (a);
+    float number3(334.23f);
+    double number4(14049.30493);
+    
+    std::cout<<number<<std::endl;
+    std::cout<<number2<<std::endl;
+    std::cout<<"a"<<std::endl;
+    std::cout<<number3<<std::endl;
+    std::cout<<number4<<std::endl;
+    
+    
+    return 0;
+}
